f7b7cfeacc61eeec1b282cb60d40b7bb5a076cda
wikimaru <64010892@kmitl.ac.th>
2025-02-02 15:47:41 +0700

    fix bug packet question

--- app/components/seer/package/question/routes.py
+++ app/components/seer/package/question/routes.py
@@ -9,7 +9,7 @@ from app.core.error import (
     NotFoundException
 )
 from app.database import SessionDep
-from app.database.models import Seer,QuestionPackage
+from app.database.models import Seer, QuestionPackage
 from app.components.seer.service import check_active_seer
 
 from . import responses as res
@@ -23,28 +23,20 @@ router_me = APIRouter(prefix="/question")
 @router_me.get("")
 async def get_QuestionPackage(payload: SeerJWTDep, session: SessionDep):
     '''
-    200 OK: return QuestionPackageIn ที่ได้รับเข้ามา
-    400 Bad Request: BadRequestException("Seer not found.")
-    (กรณีล้าง db แต่ token ยังอยู่ / ใช้ 400 front จะได้แยกออกง่าย)
-    404 Not Found: NotFoundException("QuestionPackage not found.")
+    ดูข้อมูล QuestionPackage
     '''
     user_id = payload.sub
     result = await get_questionpackage(session, user_id)
     if result is None:
         raise NotFoundException("QuestionPackage found.")
     return result
-# ดึง QuestionPackageOut ของ Seer ตัวเอง
-# Status code:
-# 200 OK: return QuestionPackageIn ที่ได้รับเข้ามา
-# 400 Bad Request: BadRequestException("Seer not found.")
-# ... (กรณีล้าง db แต่ token ยังอยู่ / ใช้ 400 front จะได้แยกออกง่าย)
-# 404 Not Found: NotFoundException("QuestionPackage not found.")
 
 
 @router_me.put("")
-async def put_QuestionPackage(payload: SeerJWTDep, session: SessionDep,data : QuestionPackageIn):
+async def put_QuestionPackage(payload: SeerJWTDep, session: SessionDep, data: QuestionPackageIn):
     '''
-    young mai dy write doc na
+    Edit Question Package set ค่าครั้งแรกต้องใส่ครบทุกช่อง
+    ครั้งต่อๆ ไปใส่แค่บางช่องก็ได้
     '''
     user_id = payload.sub
     try:
@@ -52,36 +44,22 @@ async def put_QuestionPackage(payload: SeerJWTDep, session: SessionDep,data : Qu
     except NoResultFound:
         raise NotFoundException("Seer not found.")
     pass
-    result = await edit_questionpackage(session, user_id,data)
+
+    result = await edit_questionpackage(session, user_id, data)
     if result is None:
         raise IntegrityException("Wrong Value")
     return result
-# params ใน QuestionPackageIn ทั้งหมดเป็น optional
-# (upsert / insert ... on conflict)
-# TODO: รับ QuestionPackageIn เข้ามา
-# ถ้าไม่เคยมี QuestionPackage มาก่อนให้สร้างใหม่ ควรจะ error ถ้าใส่ param ไม่ครบ
-# ถ้ามี QuestionPackage แล้วให้อัพเดทตามค่าที่ส่งเข้ามา
-# สรุปก็คือทำหน้าสร้างและแก้ไขใน api เดียว
-# Status code:
-# 200 OK: return QuestionPackageIn ที่ได้รับเข้ามา
-# 404 Not Found: NotFoundException("Seer not found.")
-# 409 Conflict (IntegrityException): error จากการ insert ครั้งแรกแล้วใส่ค่าไม่ครบ
 
 
-# /seer/{seer_id}/package/question
 router_id = APIRouter(prefix="/question")
 
 
 @router_id.get("")
-async def GetPacket(session: SessionDep,seer_id : int):
-    result = await get_questionpackage(session,seer_id)
+async def GetPacket(session: SessionDep, seer_id: int):
+    '''
+    ดูข้อมูล QuestionPackage seer id
+    '''
+    result = await get_questionpackage(session, seer_id)
     if result is None:
         raise NotFoundException("QuestionPackage not found.")
     return result
-# ดึง QuestionPackageOut ของ Seer ที่ระบุ
-# Status code:
-# 200 OK: return QuestionPackageIn ที่ได้รับเข้ามา
-# 400 Bad Request: BadRequestException("Seer not found.")
-# ... (กรณีล้าง db แต่ token ยังอยู่ / ใช้ 400 front จะได้แยกออกง่าย)
-# 404 Not Found: NotFoundException("QuestionPackage not found.")
-# ไปสร้าง func ใน service ซะไอสาสสส
--- app/components/seer/package/question/service.py
+++ app/components/seer/package/question/service.py
@@ -11,69 +11,62 @@ from .schemas import *
 async def get_questionpackage(session: AsyncSession, seer_id: int):
     stmt = (
         select(QuestionPackage).
-        where(QuestionPackage.seer_id == seer_id,QuestionPackage.id == 1)
+        where(QuestionPackage.seer_id == seer_id, QuestionPackage.id == 1)
     )
     result = (await session.execute(stmt)).scalar_one_or_none()
     return result
 
-async def check_field_questionpackagein_first(question_package_in : QuestionPackageIn):
-    return (question_package_in.price == None or 
+
+async def check_field_questionpackagein_first(question_package_in: QuestionPackageIn):
+    return (
+        not 'price' in question_package_in or
+        not 'is_enabled' in question_package_in or
+        not 'stack_limit' in question_package_in or
+        not 'image' in question_package_in or
+        not 'description' in question_package_in or
+        question_package_in.price == None or
         question_package_in.description == None or
         question_package_in.is_enabled == None or
         question_package_in.stack_limit == None or
         question_package_in.image == None
     )
 
-async def check_field_questionpackagein(question_package_in : QuestionPackageIn):
-    return (question_package_in.price == None or 
-        question_package_in.description == None or
-        question_package_in.is_enabled == None or
-        question_package_in.stack_limit == None or
-        question_package_in.image == None
-    )
-    
-async def edit_questionpackage(session : SessionDep , user_id : int , qp_data : QuestionPackageIn):
+
+async def edit_questionpackage(session: SessionDep, user_id: int, qp_data: QuestionPackageIn):
     has_questionpackage = await get_questionpackage(session, user_id)
     if has_questionpackage is None:
-        if not check_field_questionpackagein_first(qp_data):
+        if check_field_questionpackagein_first(qp_data):
             return None
         data = qp_data.model_dump()
         data["seer_id"] = user_id
         data["id"] = 1
-        if "is_enabled" in data :
+        if "is_enabled" in data:
             del data["is_enabled"]
             data["enable_at"] = qp_data.enable_at
         stmt = (
             insert(QuestionPackage).
+            values(data).returning(QuestionPackage.seer_id)
+        )
+        result = (await session.execute(stmt)).one_or_none()
+        await session.commit()
+        if result is None:
+            return None
+        return data
+    else:
+        data = qp_data.model_dump(exclude_unset=True)
+        if "is_enabled" in data:
+            del data["is_enabled"]
+            data["enable_at"] = qp_data.enable_at
+        stmt = (
+            update(QuestionPackage).
+            where(QuestionPackage.seer_id == user_id, QuestionPackage.id == 1).
             values(data).
             returning(
                 QuestionPackage.price,
                 QuestionPackage.description,
                 QuestionPackage.enable_at.label("is_enabled"),
                 QuestionPackage.stack_limit,
-                QuestionPackage.image,
-            )
-        )
-        result = (await session.execute(stmt)).one_or_none()
-        await session.commit()
-        if result is None:
-            return None
-        return QuestionPackageOut.model_validate(result)
-    else:
-        data = qp_data.model_dump(exclude_unset=True)
-        if "is_enabled" in data :
-            del data["is_enabled"]
-            data["enable_at"] = qp_data.enable_at
-        stmt = (
-        update(QuestionPackage).
-        where(QuestionPackage.seer_id == user_id,QuestionPackage.id == 1).
-        values(data).
-        returning(                
-            QuestionPackage.price,
-            QuestionPackage.description,
-            QuestionPackage.enable_at.label("is_enabled"),
-            QuestionPackage.stack_limit,
-            QuestionPackage.image
+                QuestionPackage.image
             )
         )
         result = (await session.execute(stmt)).one_or_none()
