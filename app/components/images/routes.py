690111ef3bd7f20052f382a46c7d8dcf54fc2ad4
Wikimaru <0mh19280@gmail.com>
2025-02-06 12:54:11 +0700

    Edit upload_fortune_package_image

--- app/components/images/routes.py
+++ app/components/images/routes.py
@@ -6,6 +6,7 @@ from sqlalchemy import delete, select, update, insert
 from sqlalchemy.exc import NoResultFound, IntegrityError
 
 from app.core.deps import UserJWTDep
+from app.database.models import FPStatus
 from app.core.error import (
     BadRequestException,
     NotFoundException,
@@ -43,12 +44,13 @@ async def upload_user_profile_image( payload: UserJWTDep, session: SessionDep, f
         returning(User.id)
     )
 
-    if not await UploadImage(part,file_name,file):
-        raise HTTPException(status_code=500, detail='Fail To Upload')
-    
     result = (await session.execute(stmt)).one_or_none()
     if result is None:
         raise NotFoundException("User not found.")
+
+    if not await UploadImage(part,file_name,file):
+        raise HTTPException(status_code=500, detail='Fail To Upload')
+    
     await session.commit()
     return [{"filename": file.filename},{"fileType":file.content_type},{"url": local_url}]
 
@@ -68,17 +70,20 @@ async def upload_fortune_package_image( payload: UserJWTDep, session: SessionDep
     local_url = CreateUrl(part,file_name)
     stmt = (
         update(FortunePackage).
-        where(FortunePackage.id == package_id, FortunePackage.seer_id == user_id).
+        where(FortunePackage.id == package_id, 
+              FortunePackage.seer_id == user_id,
+              FortunePackage.status == FPStatus.draft).
         values(image = local_url).
         returning(FortunePackage.id)
     )
 
+    result = (await session.execute(stmt)).one_or_none()
+    if result is None:
+        raise NotFoundException("Fortune Package not found.")
+
     if not await UploadImage(part,file_name,file):
         raise HTTPException(status_code=500, detail='Fail To Upload')
     
-    result = (await session.execute(stmt)).one_or_none()
-    if result is None:
-        raise NotFoundException("Fortune Package found.")
     await session.commit()
     return [{"filename": file.filename},{"fileType":file.content_type},{"url": local_url}]
 
@@ -103,11 +108,12 @@ async def upload_question_package_image( payload: UserJWTDep, session: SessionDe
         returning(QuestionPackage.id)
     )
 
-    if not await UploadImage(part,file_name,file):
-        raise HTTPException(status_code=500, detail='Fail To Upload')
-    
     result = (await session.execute(stmt)).one_or_none()
     if result is None:
         raise NotFoundException("Package Question not found.")
+
+    if not await UploadImage(part,file_name,file):
+        raise HTTPException(status_code=500, detail='Fail To Upload')
+    
     await session.commit()
     return [{"filename": file.filename},{"fileType":file.content_type},{"url": local_url}]
